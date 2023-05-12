2f16511aa0a7765544043ab5bd2ebc06bb414225
AuroraNewman <114418077+AuroraNewman@users.noreply.github.com>
2023-05-12 20:23:27 +0100

    Initial commit

---
+++ src/main/java/uk/ncl/CSC8016/jackbergus/coursework/project2/processes/ClientLifecycle.java
@@ -0,0 +1,93 @@
+package uk.ncl.CSC8016.jackbergus.coursework.project2.processes;
+
+import uk.ncl.CSC8016.jackbergus.coursework.project2.utils.BasketResult;
+import uk.ncl.CSC8016.jackbergus.coursework.project2.utils.UtilityMethods;
+
+import java.util.Random;
+
+/**
+ * only edit this class and the product monitor class
+ * in client lifecycle, client may be using web app online or mobile app in shop
+ * process:
+ * * client logs in using login method
+ * * * only valid users may log in
+ * * * which creates a unique transaction ID
+ * * * client may log in using same ID multiple times, but each should be new ID
+ * * add items to basket
+ * * * only as many items as are available
+ * * remove items from basket
+ * * checkout
+ * * * check the items are available here
+ * * * * if unavailable, tell the seller to order that many more
+ * * * either all or no items are bought
+ * * log out
+ * * * lose all info on items in basket
+ * * * reshelve the items
+ */
+
+public class ClientLifecycle implements Runnable {
+
+    private final String username;
+    private final RainforestShop s;
+    private final int items_to_pick_up;
+    private final double total_available_money, shelfing_prob;
+    private final Random rng;
+    private boolean doCheckOut = true;
+
+    BasketResult l;
+
+    public ClientLifecycle(String username,
+                           RainforestShop s,
+                           int items_to_pick_up,
+                           double shelfing_prob,
+                           double total_available_money,
+                           int rng_seed) {
+        this.username = username;
+        this.s = s;
+        this.items_to_pick_up = items_to_pick_up;
+        this.total_available_money = total_available_money;
+        this.shelfing_prob = Double.min(1.0, Double.max(0.0, shelfing_prob));
+        rng = new Random(rng_seed);
+    }
+
+    public Thread thread(boolean doCheckOut) {
+        this.doCheckOut = doCheckOut;
+        return new Thread(this);
+    }
+
+    public BasketResult getBasketResult() {
+        return l;
+    }
+
+    public BasketResult startJoinAndGetResult(boolean doCheckOut) throws InterruptedException {
+        this.doCheckOut = doCheckOut;
+        Thread t = new Thread(this);
+        t.start(); t.join();
+        return l;
+    }
+
+    @Override
+    public void run() {
+        l = null;
+        double nextAfter = Math.nextUp(1.0);
+        if (s != null) {
+            s.login(username).ifPresent(transaction -> {
+                for (int i = 0; i< items_to_pick_up; i++) {
+                    var obj = UtilityMethods.getRandomElement(s.getAvailableItems(transaction), rng);
+                    if (obj == null) break;
+                    if (transaction.basketProduct(obj)) {
+                        if (rng.nextDouble(0.0, nextAfter) < shelfing_prob) {
+                            if (!transaction.shelfProduct(UtilityMethods.getRandomElement(transaction.getUnmutableBasket(), rng)))
+                                throw new RuntimeException("ERROR: I musth be able to shelf a product that I added!");
+                        }
+                    }
+                }
+                if (doCheckOut)
+                    l = transaction.basketCheckout(total_available_money);
+                else
+                    l = null;
+                transaction.logout();
+            });
+        }
+    }
+}
