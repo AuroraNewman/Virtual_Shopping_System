6f16e8479df5b63ca1f9fa7c096474dba94f474c
c2031089 <c2031089@ncl.ac.uk>
2023-05-15 14:13:49 +0100

    continued work on assertions

--- src/main/java/uk/ncl/CSC8016/jackbergus/coursework/project2/processes/Assertions.java
+++ src/main/java/uk/ncl/CSC8016/jackbergus/coursework/project2/processes/Assertions.java
@@ -24,12 +24,48 @@ public class Assertions {
         }
     }
 
+    public static void assertEquals(Object expected, Object actual) {
+        try {
+            if (expected != actual)
+                throw new AssertionError(buildMessage(expected, actual));
+        } catch (AssertionError e) {
+            System.err.println("Assertion failed: " + e.getMessage());
+        }
+    }
+
+    public static void assertNotEquals(Object expected, Object actual) {
+        try {
+            if (expected == actual)
+                throw new AssertionError(buildMessage(expected, actual));
+        } catch (AssertionError e) {
+            System.err.println("Assertion failed: " + e.getMessage());
+        }
+    }
+
+    public static void assertNull(Object expected) {
+        try {
+            if (expected != null)
+                throw new AssertionError(buildMessage(null, expected));
+        } catch (AssertionError e) {
+            System.err.println("Assertion failed: " + e.getMessage());
+        }
+    }
+
+    public static void assertNotNull(Object expected) {
+        try {
+            if (expected == null)
+                throw new AssertionError(buildMessage("not null", expected));
+        } catch (AssertionError e) {
+            System.err.println("Assertion failed: " + e.getMessage());
+        }
+    }
+
     private static String buildMessage(Object expected, Object actual) {
         final StringBuilder sb = new StringBuilder("expected ");
         sb.append("\"").append(expected).append("\"");
         sb.append(", actual value is ");
         sb.append("\"").append(actual).append("\"");
 
-        return sb.toString();    }
-
+        return sb.toString();
+    }
 }
